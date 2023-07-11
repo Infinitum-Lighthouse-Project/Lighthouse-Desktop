4f66525c807b03897abb2b6cc1360fa853a619ff
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-11 18:30:28 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LighthouseApp());
+}
+
+class LighthouseApp extends StatelessWidget {
+  const LighthouseApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
