22003ce753b14cd5a3e6fe693016c3b7de833329
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-20 19:44:05 +0800

    implement `LHIcoTextButton` and cursor styles

--- lib/design_system/button.dart
+++ lib/design_system/button.dart
@@ -36,36 +36,35 @@ class LHButtonState extends State<LHButton> {
           AnimatedContainer(
             width: widget.width,
             height: widget.height,
-/*                 width: widget.width,
-                height: widget.height, */
             duration: const Duration(milliseconds: 200),
             curve: Curves.easeOutQuad,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(2),
               color: currentColor,
             ),
-            child: MouseRegion(
-              onEnter: (_) {
+            child: GestureDetector(
+              onTapDown: (_) {
+                setState(() {
+                  currentColor = widget.pressedColor;
+                });
+              },
+              onTapUp: (_) {
                 setState(() {
                   currentColor = widget.hoverColor;
                 });
+                widget.callback();
               },
-              onExit: (_) {
-                setState(() {
-                  currentColor = widget.inactiveColor;
-                });
-              },
-              child: GestureDetector(
-                onTapDown: (_) {
-                  setState(() {
-                    currentColor = widget.pressedColor;
-                  });
-                },
-                onTapUp: (_) {
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                onEnter: (_) {
                   setState(() {
                     currentColor = widget.hoverColor;
                   });
-                  widget.callback();
+                },
+                onExit: (_) {
+                  setState(() {
+                    currentColor = widget.inactiveColor;
+                  });
                 },
                 child: Padding(
                   padding: const EdgeInsets.all(4),
@@ -77,19 +76,8 @@ class LHButtonState extends State<LHButton> {
         ],
       ),
     );
-/*     return Center(
-      child: SizedBox(
-/*         width: widget.width,
-        height: widget.height, */
-        child: 
-      ),
-    ); */
   }
 }
-
-/**
- * 
- */
 
 class LHIconButton extends LHButton {
   final IconData iconData;
@@ -116,56 +104,72 @@ class LHIconButton extends LHButton {
 
 class LHTextButton extends LHButton {
   final String text;
-  final double? fixedWidth;
   final double fixedHeight;
   // final bool autoSizing;
 
   LHTextButton({
     required this.text,
     required super.callback,
-    this.fixedHeight = 24,
-    required this.fixedWidth,
+    this.fixedHeight = 28,
+    super.width,
     super.key,
   }) : super(
-          width: fixedWidth != null
-              ? (LHDesignSystem.scaleFactor * fixedWidth)
-              : null,
           height: LHDesignSystem.scaleFactor * fixedHeight,
           inactiveColor: Colors.transparent,
           hoverColor: const Mauve.s700().withOpacity(0.5),
           pressedColor: const Mauve.s700(),
           child: Center(
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Mauve.s300(),
-                fontSize: 16,
+            child: Padding(
+              padding: const EdgeInsets.all(2),
+              child: Text(
+                text,
+                style: const TextStyle(
+                  color: Mauve.s300(),
+                  fontSize: 16,
+                ),
               ),
             ),
           ),
         );
 }
 
-/**
- * Center(
-            child: Container(
-              width: LHDesignSystem.scaleFactor * 28,
-              height: LHDesignSystem.scaleFactor * 28,
-              child: TextButton(
-                onPressed: widget.onPressed,
-                style: ButtonStyle(
-                  overlayColor: MaterialStateColor.resolveWith(
-                    (states) => const Mauve.s700().withOpacity(0.5),
-                  ),
-                  splashFactory: InkSplash.splashFactory,
-                  animationDuration: const Duration(seconds: 3),
-                ),
-                child: Icon(
-                  widget.iconData,
+class LHIcoTextButton extends LHButton {
+  final String text;
+  final double fixedHeight;
+  final IconData iconData;
+
+  LHIcoTextButton({
+    required this.text,
+    required this.iconData,
+    this.fixedHeight = 24,
+    required super.callback,
+    super.width,
+    super.key,
+  }) : super(
+          height: LHDesignSystem.scaleFactor * fixedHeight,
+          inactiveColor: Colors.transparent,
+          hoverColor: const Mauve.s700().withOpacity(0.5),
+          pressedColor: const Mauve.s700(),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  iconData,
                   size: LHDesignSystem.scaleFactor * 20,
                   color: const Mauve.s300(),
                 ),
-              ),
+                const SizedBox(width: 4),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Mauve.s300(),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
             ),
           ),
- */
+        );
+}
--- lib/views/test_view.dart
+++ lib/views/test_view.dart
@@ -4,10 +4,26 @@ class TestView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewScaffold(
-      child: LHTextButton(
-        callback: () {},
-        text: 'Icon Check',
-        fixedWidth: null,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LHIconButton(
+            iconData: Icons.inbox,
+            callback: () {},
+          ),
+          const SizedBox(width: 30),
+          LHTextButton(
+            text: 'Mail',
+            callback: () {},
+          ),
+          const SizedBox(width: 30),
+          LHIcoTextButton(
+            callback: () {},
+            iconData: Icons.access_alarm_outlined,
+            text: 'Mail',
+            fixedHeight: 30,
+          ),
+        ],
       ),
     );
   }
